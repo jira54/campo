2dacf3151a4645796e8820f4f50a9f07d441c626
fanuel08 <fanueljunior08@gmail.com>
2026-04-07 03:42:17 +0300

    Fix migration script to include proper timestamp

--- fix_deploy_migration.py
+++ fix_deploy_migration.py
@@ -2,6 +2,7 @@
 import os
 import django
 from django.db import connection
+from django.utils import timezone
 
 # Set up Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -18,11 +19,11 @@ def fix_contenttypes_migration():
         count = cursor.fetchone()[0]
         
         if count == 0:
-            # Mark the migration as applied
+            # Mark the migration as applied with proper timestamp
             cursor.execute("""
-                INSERT INTO django_migrations (app, name) 
-                VALUES ('contenttypes', '0002_remove_content_type_name')
-            """)
+                INSERT INTO django_migrations (app, name, applied) 
+                VALUES ('contenttypes', '0002_remove_content_type_name', %s)
+            """, [timezone.now()])
             print("Marked contenttypes.0002_remove_content_type_name as applied")
         else:
             print("contenttypes.0002_remove_content_type_name already marked as applied")

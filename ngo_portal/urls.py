62958aec92f5fccf22d68b37818dbd009ff20fc4
fanuel08 <fanueljunior08@gmail.com>
2026-04-11 15:19:15 +0300

    feat: Inject NGO functional data-entry views and templates (Register Life, Mission Tracking)

--- ngo_portal/urls.py
+++ ngo_portal/urls.py
@@ -6,5 +6,8 @@ app_name = 'ngo_portal'
 urlpatterns = [
     path('dashboard/', views.ngo_dashboard, name='dashboard'),
     path('beneficiaries/', views.beneficiary_list, name='beneficiaries'),
+    path('beneficiaries/add/', views.add_beneficiary, name='add_beneficiary'),
+    path('programs/add/', views.add_program, name='add_program'),
+    path('activity/log/', views.log_activity, name='log_activity'),
     path('export/audit/', views.export_donor_audit, name='export_audit'),
 ]
